8f21292d860898d7f3ba7f6907aeba114eb979ce
hewettp <peter@hewett.co.nz>
2021-09-04 20:52:36 +1200

    first draft

---
+++ your-turn/03-source-control/game.py
@@ -0,0 +1,20 @@
+import random
+
+
+def main(number):
+    print("Welcome to the HI - LO game")
+    turns = 6
+    while turns > 0:
+        response = int(input('Guess a number between 1 & 100: '))
+        if response == number:
+            return f"Got it! The number is {number}"
+        if response > number:
+            print('Too high!')
+        else:
+            print('Too low!')
+        turns += 1
+    return 'You lost'
+
+
+if __name__ == '__main__':
+    print(main(random.randint(1, 100)))
